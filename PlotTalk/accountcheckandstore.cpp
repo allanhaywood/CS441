9fc26edaa2c033fa98e7891e45e47eb2b33a890b
Joshua Reaves <reave006@cougars.csusm.edu>
2016-10-11 21:50:35 -0700

    Adding a class for holding and storing user account info (will need to be ammended for server)

---
+++ PlotTalk/accountcheckandstore.cpp
@@ -0,0 +1,6 @@
+#include "accountcheckandstore.h"
+
+AccountCheckAndStore::AccountCheckAndStore()
+{
+
+}
---
+++ PlotTalk/accountcheckandstore.h
@@ -0,0 +1,15 @@
+#ifndef ACCOUNTCHECKANDSTORE_H
+#define ACCOUNTCHECKANDSTORE_H
+
+#include <QDeclarativeItem>
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+
+class AccountCheckAndStore
+{
+public:
+    AccountCheckAndStore();
+};
+
+#endif // ACCOUNTCHECKANDSTORE_H
