7f5314e3acddbfe86c88a677b48ee4e197bee959
Joey Freeman <joseph.freeman.sd@gmail.com>
2016-11-05 16:22:24 -0700

    Updated Reactions classes and UML
    -Replies can not be added to replies
    -Changed userName to userID
    -Moved reply vector and addReply() method from Reaction to Review and
    Comment subclasses
    -Updated tests to work with changes
    -Updated UML to reflect changes and relationships

--- PlotTalk/Src/comment.h
+++ PlotTalk/Src/comment.h
@@ -1,12 +1,15 @@
 #ifndef COMMENT_H
 #define COMMENT_H
 #include "reaction.h"
+#include "reply.h"
 
 class Comment : public Reaction
 {
 public:
     Comment();
-    Comment(int inMediaID, QString inUserName, QString inText);
+    Comment(int inMediaID, int inUserID, QString inText);
+    QVector<Reply> replies;
+    void addReply(Reply);
 };
 
 #endif // COMME_H
--- PlotTalk/Test/testreply.cpp
+++ PlotTalk/Test/testreply.cpp
@@ -8,28 +8,29 @@ void TestReply::TestCreateReply()
     // Set up values to compare against
     int mediaID = 10;
     QString currentDateTime = QDateTime::currentDateTimeUtc().toString("MM/dd/yyyy h:m ap UTC");
-    QString userNameReview = "user1";
+    int userIDReview = 1;
     int rating = 1;
     QString textReview = "I hate this show.";
-    QString userNameReply = "user2";
+    int userIDReply = 2;
     QString textReply = "This show is my life.";
 
-    Review review(10, "user1", "I hate this show.", 1);
+    Review review(10, 1, "I hate this show.", 1);
     review.setDateTimePosted();
-    Reply reply(10, "user2", "This show is my life.");
+    Reply reply(10, 2, "This show is my life.");
     reply.setDateTimePosted();
-    review.addReply(reply.postID);
+    review.addReply(reply);
+    Reply postedReply = review.replies[0];
 
-    QCOMPARE(review.userName, userNameReview);
+    QCOMPARE(review.userID, userIDReview);
     QCOMPARE(review.mediaID, mediaID);
     QCOMPARE(review.text, textReview);
     QCOMPARE(review.dateTimePosted, currentDateTime);
     QCOMPARE(review.rating, rating);
-    QCOMPARE(review.replies[0], reply.postID);
-    QCOMPARE(reply.userName, userNameReply);
-    QCOMPARE(reply.mediaID, mediaID);
-    QCOMPARE(reply.text, textReply);
-    QCOMPARE(reply.dateTimePosted, review.dateTimePosted);
+    QCOMPARE(postedReply.mediaID, reply.mediaID);
+    QCOMPARE(postedReply.userID, userIDReply);
+    QCOMPARE(postedReply.mediaID, mediaID);
+    QCOMPARE(postedReply.text, textReply);
+    QCOMPARE(postedReply.dateTimePosted, review.dateTimePosted);
     qDebug() << "Review and reply created at " << reply.dateTimePosted;
 
 }
